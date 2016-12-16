d06bb98134f9b93e6eaeba57dbda1d75a880dfa5
Efim Polevoi <fimkap@MacBook-Pro.local>
2016-12-16 02:00:30 +0100

    NavBar and style.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @articles = Article.all
+        @articles = Article.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
         @articles_count = Article.count
     end
 
